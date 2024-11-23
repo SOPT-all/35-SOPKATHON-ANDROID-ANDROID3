d7a195ae011f48d708b901dbc2a959f16564410b
Jong <jm991014@gmail.com>
2024-11-24 07:24:38 +0900

    #29 [feat] : Splash Screen 구현

--- app/src/main/java/com/example/sopkathon_android_team3/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/sopkathon_android_team3/presentation/main/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
+import com.example.sopkathon_android_team3.presentation.splash.SplashScreen
 import com.example.sopkathon_android_team3.ui.theme.SOPKATHON_ANDROID_TEAM3Theme
 
 class MainActivity : ComponentActivity() {
@@ -13,9 +18,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+            var showSplash by remember { mutableStateOf(true) }
 
             SOPKATHON_ANDROID_TEAM3Theme {
-                MainScreen(navController = navController)
+                if (showSplash) {
+                    SplashScreen {
+                        showSplash = false
+                    }
+                } else {
+                    MainScreen(navController = navController)
+                }
             }
         }
     }
