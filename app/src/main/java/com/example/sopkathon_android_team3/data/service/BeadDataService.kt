e0e9d85ea26a448c03666442d5b88181204db9d7
beom84 <cjja00@konkuk.ac.kr>
2024-11-24 05:35:19 +0900

    #21 [feat] : API Dto 연결2

--- app/src/main/java/com/example/sopkathon_android_team3/data/service/BeadDataService.kt
+++ app/src/main/java/com/example/sopkathon_android_team3/data/service/BeadDataService.kt
@@ -1,14 +1,12 @@
 package com.example.sopkathon_android_team3.data.service
 
 import com.example.sopkathon_android_team3.data.dto.request.RequestBeadContent
-import com.example.sopkathon_android_team3.data.dto.request.RequestKingBeadContent
 import com.example.sopkathon_android_team3.data.dto.response.ResponseBeadContent
 import com.example.sopkathon_android_team3.data.dto.response.ResponseRandomBeadContent
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface BeadDataService {
     @POST("/bead")
@@ -20,7 +18,6 @@ interface BeadDataService {
     @POST("/kingbead")
     suspend fun postKingBeadContent(
         @Header("userId") userId: Long,
-        @Body request: RequestKingBeadContent
     ): ResponseBeadContent
 
     @GET("/bead")
