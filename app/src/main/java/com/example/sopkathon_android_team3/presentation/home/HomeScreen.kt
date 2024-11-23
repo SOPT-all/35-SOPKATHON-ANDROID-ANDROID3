2c50df1a9d159063dd8aaed4126e886db1c0626a
Jong <jm991014@gmail.com>
2024-11-24 04:10:02 +0900

    #14 [feat] : home description dialog 추가

--- app/src/main/java/com/example/sopkathon_android_team3/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/sopkathon_android_team3/presentation/home/HomeScreen.kt
@@ -2,11 +2,17 @@ package com.example.sopkathon_android_team3.presentation.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.sopkathon_android_team3.presentation.home.component.HomeDescriptionDialog
 import com.example.sopkathon_android_team3.ui.theme.SOPKATHON_ANDROID_TEAM3Theme
 import com.example.sopkathon_android_team3.ui.theme.SopkathonAndroidTeam3Theme
 
@@ -15,6 +21,15 @@ fun HomeRoute(
     padding: PaddingValues,
     onNavigateToStorage: () -> Unit
 ) {
+    var showDialog by remember { mutableStateOf(true) }
+
+    if (showDialog) {
+        HomeDescriptionDialog(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onDismissRequest = { showDialog = false }
+        )
+    }
     HomeScreen(
         modifier = Modifier.padding(padding),
         onNavigateToStorage = onNavigateToStorage
@@ -26,6 +41,8 @@ private fun HomeScreen(
     onNavigateToStorage: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+
     Text(
         modifier = modifier.clickable(onClick = onNavigateToStorage),
         text = "This is Home Screen",
