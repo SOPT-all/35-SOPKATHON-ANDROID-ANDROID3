7ecc1c6c82bef35590bb46f69ac9aa4e16d27972
Jong <jm991014@gmail.com>
2024-11-24 05:11:43 +0900

    #19 [feat] : home view 구현

--- app/src/main/java/com/example/sopkathon_android_team3/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/sopkathon_android_team3/presentation/home/HomeScreen.kt
@@ -1,10 +1,16 @@
 package com.example.sopkathon_android_team3.presentation.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -12,13 +18,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.sopkathon_android_team3.R
 import com.example.sopkathon_android_team3.presentation.component.BasicLottie
 import com.example.sopkathon_android_team3.presentation.home.component.HomeDescriptionDialog
 import com.example.sopkathon_android_team3.presentation.type.CrystalAnimation
 import com.example.sopkathon_android_team3.ui.theme.SOPKATHON_ANDROID_TEAM3Theme
+import com.example.sopkathon_android_team3.ui.theme.SopkathonAndroidTeam3Theme
+import com.example.sopkathon_android_team3.util.modifier.noRippleClickable
 
 @Composable
 fun HomeRoute(
@@ -37,24 +53,96 @@ fun HomeRoute(
             onDismissRequest = { showDialog = false }
         )
     }
+    Box {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.img_background_home),
+            contentDescription = null
+        )
+        HomeScreen(
+            onClickDescription = {
+                showDialog = true
+            },
+            onNavigateToStorage = onNavigateToStorage,
+            animation = animation,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+        )
+    }
+
     LaunchedEffect(Unit) {
         viewModel.fetchAnimation(count = 3)
     }
-    HomeScreen(
-        modifier = Modifier.padding(padding),
-        animation = animation,
-        onNavigateToStorage = onNavigateToStorage,
-    )
 }
 
 @Composable
 private fun HomeScreen(
+    onClickDescription: () -> Unit,
     onNavigateToStorage: () -> Unit,
     animation: CrystalAnimation,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier.fillMaxSize()) {
-        BasicLottie(crystalAnimation = animation.jsonFileName)
+    Row(
+        modifier = modifier.padding(top = 42.dp, start = 18.dp, end = 13.dp)
+    ) {
+        Icon(
+            modifier = Modifier.padding(top = 9.dp),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_logo),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Column(
+            modifier = Modifier
+                .noRippleClickable(onClickDescription)
+                .padding(end = 17.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_crystalball_small),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+            Row(
+                modifier = Modifier.padding(top = 5.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.home_crystal_ball),
+                    style = SopkathonAndroidTeam3Theme.typography.body4SemiBold11,
+                    color = SopkathonAndroidTeam3Theme.colors.white
+                )
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_crystalball_question),
+                    contentDescription = null,
+                    tint = SopkathonAndroidTeam3Theme.colors.white
+                )
+            }
+        }
+        Column(
+            modifier = Modifier.noRippleClickable(onNavigateToStorage),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_storagebox),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+            Text(
+                modifier = Modifier.padding(top = 5.dp),
+                text = stringResource(R.string.home_crystal_storage),
+                style = SopkathonAndroidTeam3Theme.typography.body4SemiBold11,
+                color = SopkathonAndroidTeam3Theme.colors.white
+            )
+        }
+    }
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        BasicLottie(
+            crystalAnimation = animation.jsonFileName
+        )
     }
 }
 
